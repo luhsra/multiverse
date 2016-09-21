e9f41fc41689b2b8b805390d2467c837430f46da
Valentin Rothberg <valentinrothberg@gmail.com>
2016-09-21 14:00:29 +0200

    multiverse attribute registration
    The multiverse attribute can now be used with the GCC plugin.  GCC does
    not complain anymore about not knowing the attribute.  The next step is
    to find references on multiverse variables.

--- src/multiverse.c
+++ src/multiverse.c
@@ -1,30 +1,48 @@
 /*
  * Licensed under the GPL v2, or (at your option) v3
+ *
+ *
+ * Further reading for function/compiler multiverse:
+ *
+ *  * "Function Multiverses for Dynamic Variability"
+ *     https://www4.cs.fau.de/Publications/2016/rothberg_16_dspl.pdf
+ *
+ *  * Kernel Newbies wiki
+ *    https://kernelnewbies.org/KernelProjects/compiler-multiverse 
  */
 
+
 #include <gcc-plugin.h>
+#include <tree.h>
 #include <tree-pass.h>
 
-#define HELPMESSAGE "There you go"
+extern void print_generic_stmt(FILE *, tree, int);
 
+#define DEBUG
+
+/* 
+ * All plugins must export this symbol so that they can be loaded with GCC.
+ */
 int plugin_is_GPL_compatible;
+
 
 /*
  * The following data is required for 'gcc --version / --help' and is thus
  * useful to expose the interface of the plugin and further information to the
  * user.
  */
-static struct plugin_info mv_plugin_info =
+struct plugin_info mv_plugin_info =
 {
-    .version = "early bird",
-    .help = HELPMESSAGE,
+    .version = "0.1",
+    .help = "Here should be something written about how to use this plugin.",
 };
+
 
 /*
  * Specify which version(s) of the GCC are compatible to this plugin.  There are
  * more members, such as time stamp or revision, to further narrow
  * compatibility.  Look into 'plugin-version.h' to check the fields of your
- * infrastructure.
+ * infrastructure if compatibility problems occur.
  */
 static struct plugin_gcc_version mv_plugin_version =
 {
@@ -32,21 +50,70 @@ static struct plugin_gcc_version mv_plugin_version =
 };
 
 
-static bool mv_gimple_gate()
+/*
+ * Handler of the multiverse attribute.  Currently it's doing nothing but to
+ * return NULL_TREE, but maybe we need to do something at a later point.
+ */
+
+static tree handle_mv_attribute(tree *node, tree name, tree args, int flags, bool *no_add_attrs)
 {
-    return true;
+#ifdef DEBUG
+    fprintf(stderr, "Found attribute\n");
+    fprintf(stderr, "\tvariable name = ");
+    print_generic_stmt(stderr, *node, 0);
+    fprintf(stderr, "\tattribute = ");
+    print_generic_stmt(stderr, name, 0);
+#endif
+    return NULL_TREE;
 }
 
 
-static unsigned int mv_gimple_execute()
+/*
+ * The struct below is required to register the __attribute__((multiverse))
+ * attribute.  Please refer to tree-core.h for detailed information about this
+ * struct and its members.
+ */
+static struct attribute_spec mv_attribute =
 {
-    printf("...mv_gimple_execute()\n");
-    return 0;
+    .name = "multiverse",
+    .min_length = 0,
+    .max_length = 0,
+    .decl_required = false,
+    .type_required = false,
+    .function_type_required = false,
+    .handler = handle_mv_attribute,
+    .affects_type_identity = false,
+};
+
+
+/*
+ * The callback function to register the multiverse attribute.
+ */
+static void register_mv_attribute(void *event_data, void *data)
+{
+    register_attribute(&mv_attribute);
 }
 
 
+/*
+ * Initialization function of this plugin: the very heart & soul.
+ */
 int plugin_init(struct plugin_name_args *info, struct plugin_gcc_version *ver)
 {
-    printf("plugin_init");
+#ifdef DEBUG
+    printf("Initializing the multiverse GCC plugin.\n");
+#endif
+
+    char *plugin_name = info->base_name;
+
+    // register plugin information
+    register_callback(plugin_name, PLUGIN_INFO, NULL, &mv_plugin_info);
+
+    // register the 'multiverse' attribute
+    register_callback(plugin_name, PLUGIN_ATTRIBUTES, register_mv_attribute, NULL);
+
+#ifdef DEBUG
+    printf("Initializing finished.\n");
+#endif
     return 0;
 }
--- src/test.c
+++ src/test.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+typedef enum { false, true } bool;
+
+static __attribute__((multiverse)) bool configA = true;
+
 int main(int argc, char **argv)
 {
     printf("Hallo Welt\n");
+    if (configA)
+        printf("configA\n");
+    else
+        printf("!~configA\n");
     return 0;
 }
