1c2e013c9cc488a7eb84401e3bdecdb34ab78c79
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2016-11-11 17:21:50 +0100

    tests: first actual functional test

--- tests/one-variable.c
+++ tests/one-variable.c
@@ -4,15 +4,17 @@
 
 __attribute__((multiverse)) unsigned char config;
 
-int __attribute((multiverse)) foo()
-{
-    if (config) {
-        printf("config = true\n");
-    } else {
-        printf("config = false\n");
-    }
+int copy_config;
 
-    return 42;
+int __attribute((multiverse, noinline)) foo()
+{
+    /* if (config) {
+        printf("config = true, %d\n", config);
+    } else {
+        printf("config = false, %d\n", config);
+        } */
+
+    return config;
 }
 
 
@@ -20,7 +22,9 @@ int main(int argc, char **argv)
 {
     multiverse_init();
 
+
     multiverse_dump_info(stderr);
+
 
     struct mv_info_var *var_info = multiverse_info_var(&config);
     assert(var_info != NULL);
@@ -29,16 +33,20 @@ int main(int argc, char **argv)
     assert(foo_info != NULL);
     assert(foo_info->n_mv_functions == 2);
 
-    foo();
+    /* Check the static property of multiverse variants */
+    assert(foo() == 0 && config == 0);
 
     multiverse_commit_fn(foo_info);
 
-    config = 1;
-    foo();
+    assert(foo() == 0 && config == 0);
+
+    config=1;
+
+    assert(foo() == 0 && config == 1);
 
     multiverse_commit();
 
-    foo();
+    assert(foo() == 1 && config == 1);
 
     return 0;
 }
