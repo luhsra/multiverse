096ff6485a505c2fe54429f18fedfc366a56d0d0
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2016-11-15 15:17:08 +0100

    tests: test case for function pointers

--- tests/function-pointer.c
+++ tests/function-pointer.c
@@ -9,21 +9,18 @@ int __attribute((multiverse, noinline)) foo()
     return config;
 }
 
+int (*foo_ptr)() = foo;
+
 int main(int argc, char **argv)
 {
     multiverse_init();
 
     /* Check the static property of multiverse variants */
     multiverse_commit_var(&config);
-    config = 1; assert(foo() == 0 && config == 1);
+    config = 1; assert(foo_ptr() == 0 && config == 1);
 
-    multiverse_commit_var(&config);
-    config = 0; assert(foo() == 1 && config == 0);
-
-    /* After revert the function should be flexible again. */
-    multiverse_revert_fn(&foo);
-    config = 0; assert(foo() == 0 && config == 0);
-    config = 1; assert(foo() == 1 && config == 1);
+    multiverse_commit_fn(&foo);
+    config = 0; assert(foo_ptr() == 1 && config == 0);
 
     return 0;
 }
