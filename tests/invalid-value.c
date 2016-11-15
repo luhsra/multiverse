68829818c990e00c76966ee51a50bbcdf5068123
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2016-11-15 15:26:14 +0100

    tests: testcase for a invalid value
    If there is no multiverse variant for a variable assignment, we fall
    back to the unmodified variant.

---
+++ tests/invalid-value.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "multiverse.h"
+#include "testsuite.h"
+
+__attribute__((multiverse)) unsigned char config;
+
+int __attribute((multiverse, noinline)) foo()
+{
+    return config;
+}
+
+int main(int argc, char **argv)
+{
+    multiverse_init();
+
+    /* Check the static property of multiverse variants */
+    multiverse_commit_var(&config);
+    config = 23; assert(foo() == 0 && config == 23);
+
+    multiverse_commit_var(&config);
+    assert(foo() == 23 && config == 23);
+    config = 42; assert(foo() == 42 && config == 42);
+
+    /* After revert the function should be flexible again. */
+    int x = multiverse_revert_fn(&foo);
+    assert(x == 0);
+
+    return 0;
+}
