f03ade11b67134505b92e23d3f5c50154eec9c32
Valentin Rothberg <valentinrothberg@gmail.com>
2016-11-22 15:58:47 +0100

    add tests/enums-simple.c
    Looking at the main() of this testcase, we need to add support for
    tracking the state of multiverse variables (i.e., distinguish between a
    variable being stable and unstable).

---
+++ tests/enums-simple.c
@@ -0,0 +1,41 @@
+/*
+ * Use multiverse_commit_var(&variable) to commit that a multiverse variable is
+ * stable.  This function returns the number of changed functions and -1 in case
+ * of error.
+ *
+ * TODO: add multiverse-tracked semantic
+ */
+
+#include <stdio.h>
+#include "multiverse.h"
+#include "testsuite.h"
+
+typedef enum { a=0, b, c, d, e } state;
+
+
+__attribute__((multiverse)) state configA;
+__attribute__((multiverse)) state configB;
+
+
+int __attribute((multiverse)) foo()
+{
+    printf("configA=%d, configB=%d\n", configA, configB);
+    return configA + configB;
+}
+
+
+int main(int argc, char **argv)
+{
+    multiverse_init();
+
+    configA = a; // 0
+    multiverse_commit_var(&configA);  // TODO: add multiverse-tracked semantic
+    foo();      // configA=0, configB=0
+
+    configB = b; // 1
+    foo();      // configA=0, configB=1
+    multiverse_commit_var(&configB);
+    foo();      // configA=0, configB=1
+
+    return 0;
+}
