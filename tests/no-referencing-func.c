c7d62d3a6d35e9fa680f7646b3902251d8d3fc4d
Valentin Rothberg <valentinrothberg@gmail.com>
2016-11-22 14:09:02 +0100

    tests: add no-referencing-func.c
    Nothing should happen if we commit a multiverse variable that is not
    (yet) referenced by any multiverse function.

---
+++ tests/no-referencing-func.c
@@ -0,0 +1,24 @@
+/*
+ * Nothing should happen if we commit a multiverse variable that is not (yet)
+ * referenced by any function.
+ */
+
+#include <stdio.h>
+#include "multiverse.h"
+#include "testsuite.h"
+
+__attribute__((multiverse)) unsigned char config;
+
+
+int main(int argc, char **argv)
+{
+    multiverse_init();
+
+    int x = 0;
+    config = 1;
+
+    x = multiverse_commit_var(&config);
+    assert(x == 0);
+
+    return 0;
+}
