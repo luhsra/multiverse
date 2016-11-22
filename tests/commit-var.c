4783fddc1914890af6f0ca7f771b42127eb568b4
Valentin Rothberg <valentinrothberg@gmail.com>
2016-11-22 16:04:25 +0100

    tests/commit-var.c: some comments

--- tests/commit-var.c
+++ tests/commit-var.c
@@ -1,38 +1,39 @@
+/*
+ * Use multiverse_commit_var(&variable) to commit that a multiverse variable is
+ * stable.  This function returns the number of changed functions and -1 in case
+ * of error.
+ */
+
 #include <stdio.h>
 #include "multiverse.h"
 #include "testsuite.h"
 
 __attribute__((multiverse)) unsigned char config;
 
-int copy_config;
 
-int __attribute((multiverse, noinline)) foo()
+int __attribute((multiverse)) foo()
 {
-    /* if (config) {
-       printf("config = true, %d\n", config);
-       } else {
-       printf("config = false, %d\n", config);
-       } */
-
     return config;
 }
 
 
 int main(int argc, char **argv)
 {
+    int num_changed_funcs = 0;
+
     multiverse_init();
 
     /* Check the static property of multiverse variants */
     assert(foo() == 0 && config == 0);
 
     config = 1;
-    int x = multiverse_commit_var(&config);
-    assert(x == 1);
+    num_changed_funcs = multiverse_commit_var(&config);
+    assert(num_changed_funcs == 1);
 
     assert(foo() == 1 && config == 1);
 
-    x = multiverse_commit_var(&config);
-    assert(x == 0);
+    num_changed_funcs = multiverse_commit_var(&config);
+    assert(num_changed_funcs == 0);
 
     return 0;
 }
