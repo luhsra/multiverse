dd13713b6b331807aad9c13f3a6e26bd3e335393
Valentin Rothberg <valentinrothberg@gmail.com>
2016-11-22 13:19:27 +0100

    tests/function-pointer.c: add some comments

--- tests/function-pointer.c
+++ tests/function-pointer.c
@@ -1,15 +1,23 @@
+/*
+ * The generic version of a multiverse function must also be patched to jump to
+ * the specialized version, since function pointers may still point to it.
+ */
+
 #include <stdio.h>
 #include "multiverse.h"
 #include "testsuite.h"
 
 __attribute__((multiverse)) unsigned char config;
 
+
 int __attribute((multiverse, noinline)) foo()
 {
     return config;
 }
 
-int (*foo_ptr)() = foo;
+
+int (*foo_ptr)() = foo; // function pointer on foo()
+
 
 int main(int argc, char **argv)
 {
