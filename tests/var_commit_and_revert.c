7f69a7c283b1db1e1dd9bb8200955ad4405edbde
Valentin Rothberg <valentinrothberg@gmail.com>
2016-11-22 13:03:20 +0100

    tests: add var_commit_and_revert.c
    This test is a show case for how committing and reverting multiverse
    variables works.

---
+++ tests/var_commit_and_revert.c
@@ -0,0 +1,48 @@
+/*
+ * Show how committing & reverting a multiverse variable works.
+ *
+ * The expected output is:
+ *      configA=1  | generic
+ *      configA=0  | committed and optimized
+ *      configA=0  | still optimized although configA=1
+ *      configA=1  | reverted to generic
+ */
+
+#include <stdio.h>
+#include "multiverse.h"
+#include "testsuite.h"
+
+typedef enum { false, true } bool;
+
+bool __attribute__((multiverse)) configA;
+
+
+void __attribute((multiverse)) foo()
+{
+    printf("foo(): ");
+    if (configA)
+        printf("configA=1\n");
+    else
+        printf("configA=0\n");
+}
+
+
+int main(int argc, char **argv)
+{
+    multiverse_init();
+
+    configA = 1;
+    foo();  // generic function
+
+    configA = 0;
+    multiverse_commit_var(&configA);
+    foo();  // optimized version for configA=0
+
+    configA = 1;
+    foo();  // optimized version for configA=0, although configA is now 1
+
+    multiverse_revert_var(&configA);
+    foo();  // generic function
+
+    return 0;
+}
