9bca2b89e2bbdda787640c328375fc0e31549334
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2016-11-22 16:58:21 +0100

    make: implement build system with dependencies

---
+++ common.mk
@@ -0,0 +1,37 @@
+DEPDIR = .d
+$(shell mkdir -p $(DEPDIR) >/dev/null)
+POSTCOMPILE = mv -f $(DEPDIR)/$*.Td $(DEPDIR)/$*.d
+CFLAGS+=-MT $@ -MMD -MP -MF $(DEPDIR)/$*.Td
+CXXFLAGS+=-MT $@ -MMD -MP -MF $(DEPDIR)/$*.Td
+OBJECTS = $(patsubst %,%.o,$(basename $(SOURCES)))
+
+ALLPRODUCTS += $(OBJECTS)
+
+define BINARY_template =
+$(1): $(1).o $$($(1)_OBJS) $$(EXTRA_DEP)
+	$$(CC)  -o $$@ $(1).o $$($(1)_OBJS) $$(LDFLAGS)
+
+$(1).S: $(1).c $$(EXTRA_DEPS)
+	$$(CC)  -S -o $$@ $$(CFLAGS) $$<
+
+ALLPRODUCTS += $(1).o $$($(1)_OBJS) $(1).S $(1) $(DEPDIR)/.d/$(1).d
+endef
+
+%.o : %.c
+%.o : %.c $(DEPDIR)/%.d
+	$(CC) -o $@ -c $< $(CFLAGS)
+	@$(POSTCOMPILE)
+
+%.o : %.cc
+%.o : %.cc $(DEPDIR)/%.d
+	$(CXX) -o $@ -c $< $(CXXFLAGS)
+	@$(POSTCOMPILE)
+
+$(DEPDIR)/%.d: ;
+.PRECIOUS: $(DEPDIR)/%.d
+
+clean: defaultclean
+defaultclean:
+	rm -f $(ALLPRODUCTS) core
+
+-include $(patsubst %,$(DEPDIR)/%.d,$(basename $(SOURCES)))
