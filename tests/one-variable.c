eafccb43cd6fe4554ebcd7439788d9bac4ef3d00
Valentin Rothberg <valentinrothberg@gmail.com>
2017-02-13 16:34:04 +0100

    tests: one-variable.c: add some notes and comments

--- tests/one-variable.c
+++ tests/one-variable.c
@@ -1,19 +1,20 @@
+/*
+ * A very simple testcase with
+ *  - one boolean variable 'config'
+ *  - one function 'foo' that returns 'config'
+ */
+
 #include <stdio.h>
 #include "multiverse.h"
 #include "testsuite.h"
 
-__attribute__((multiverse)) unsigned char config;
+typedef enum {true, false} bool;
 
-int copy_config;
+__attribute__((multiverse)) bool config;
+
 
 int __attribute((multiverse, noinline)) foo()
 {
-    /* if (config) {
-        printf("config = true, %d\n", config);
-    } else {
-        printf("config = false, %d\n", config);
-        } */
-
     return config;
 }
 
@@ -22,28 +23,35 @@ int main(int argc, char **argv)
 {
     multiverse_init();
 
-
-    multiverse_dump_info(stderr);
-
-
+    // Get the mv_info_var struct for config
     struct mv_info_var *var_info = multiverse_info_var(&config);
     assert(var_info != NULL);
 
+    /*
+     * Get the mv_info_fn struct for foo().  Since the mv_variable config is an
+     * "untracked" bool, there must be two mv_functions referencing this
+     * variable (config=0 and config=1).
+     */
     struct mv_info_fn *foo_info = multiverse_info_fn(&foo);
     assert(foo_info != NULL);
     assert(foo_info->n_mv_functions == 2);
 
-    /* Check the static property of multiverse variants */
+    // Check the static property of multiverse variants
     assert(foo() == 0 && config == 0);
 
     multiverse_commit_info_fn(foo_info);
 
     assert(foo() == 0 && config == 0);
 
+    /*
+     * Change config's value. foo() must still return the old value (0) since we
+     * did not commit the new value.
+     */
     config = 1;
 
     assert(foo() == 0 && config == 1);
 
+    // Call multiverse_commit() to commit config's new value
     multiverse_commit();
 
     assert(foo() == 1 && config == 1);
