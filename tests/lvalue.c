e1fccbe68b7af932574432c0a8cb0a1d8e041da9
Valentin Rothberg <valentinrothberg@gmail.com>
2017-02-14 13:12:18 +0100

    tests: lvalue.c: add description

--- tests/lvalue.c
+++ tests/lvalue.c
@@ -1,27 +1,27 @@
 /*
- * If a multiverse variable is the lvalue of an assignment, it must be removed
- * from the variable setting of the function.
+ * If a multiverse variable is the lvalue of an assignment, the function won't
+ * be specialized for this specific variable.  The compiler throws a warning in
+ * such case, since it should be discouraged to set multiverse variables
+ * _within_ a multiverse function.
  */
 
-
-#include <assert.h>
-#include <stdio.h>
-
 #include "multiverse.h"
+#include "testsuite.h"
 
 typedef enum { false, true } bool;
 
-bool __attribute__((multiverse)) configA;
+
+bool __attribute__((multiverse)) conf_a;
 
 
 void __attribute__((multiverse)) lvalue_func()
 {
     int foo = 1;
 
-    if (configA)
-        foo = configA;
+    if (conf_a)
+        foo = conf_a;
 
-    configA = false;
+    conf_a = false;  // the compiler will throw a warning for this line
 }
 
 
