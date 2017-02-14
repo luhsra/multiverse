11e6b0c6ec1307a80298b386288925efa7403de7
Valentin Rothberg <valentinrothberg@gmail.com>
2017-02-14 11:31:47 +0100

    tests: revert.c: test the _revert* interfaces
    Add rather verbose comments to improve comprehension.

--- tests/revert.c
+++ tests/revert.c
@@ -1,29 +1,83 @@
+/*
+ * The run-time library offers the possibility to revert functions to their
+ * generic versions via:
+ *  - multiverse_revert_fn(&func) : revert function 'func'
+ *  - multiverse_revert_refs(&var): revert all functions referencing 'var'
+ *  - multiverse_revert(void)     : revert all functions unconditionally
+ *
+ * This test demonstrates the semantics of those three revert functions.
+ */
+
 #include <stdio.h>
 #include "multiverse.h"
 #include "testsuite.h"
 
-__attribute__((multiverse)) unsigned char config;
+typedef enum {true, false} bool;
 
-int __attribute((multiverse, noinline)) foo()
+__attribute__((multiverse)) bool conf_a;
+__attribute__((multiverse)) bool conf_b;
+
+
+int __attribute((multiverse)) func_a()
 {
-    return config;
+    return conf_a;
 }
+
+
+int __attribute((multiverse)) func_b()
+{
+    return conf_b;
+}
+
+
+int __attribute((multiverse)) func_c()
+{
+    return conf_b || conf_a;
+}
+
 
 int main(int argc, char **argv)
 {
     multiverse_init();
 
-    /* Check the static property of multiverse variants */
-    multiverse_commit_refs(&config);
-    config = 1; assert(foo() == 0 && config == 1);
+    // Check the static property of multiverse variants
+    multiverse_commit_refs(&conf_a);
+    assert(func_a() == 0 && conf_a == 0);
 
-    multiverse_commit_refs(&config);
-    config = 0; assert(foo() == 1 && config == 0);
+    multiverse_commit_refs(&conf_b);
+    assert(func_b() == 0 && conf_b == 0);
 
-    /* After revert the function should be flexible again. */
-    multiverse_revert_fn(&foo);
-    config = 0; assert(foo() == 0 && config == 0);
-    config = 1; assert(foo() == 1 && config == 1);
+    // Use *_revert_fn() to revert a specific functions
+    multiverse_revert_fn(&func_a);
+    assert(func_a() == 0 && conf_a == 0);
+    conf_a = 1; assert(func_a() == 1);
+
+    multiverse_commit_refs(&conf_a);
+    conf_a = 0; assert(func_a() == 1);
+
+    // Use *_revert_refs() to revert referencing functions
+    assert(func_c() == 1);  // conf_a=1, conf_b=0
+    conf_a = 1; conf_b = 1; assert(func_c() == 1);  // it's still committed
+    multiverse_revert_refs(&conf_b);  // func_b & func_c will be reverted
+    assert(func_b() == 1 && func_c() == 1);
+
+    // Use *_revert() to revert all functions
+    conf_a = 0; conf_b = 1;
+    multiverse_commit();  // first commit everything
+    assert(func_a() == 0 && func_b() == 1 && func_c() == 1);
+
+    // now revert all functions and test each of them
+    multiverse_revert();
+    conf_a = 0; assert(func_a() == 0);
+    conf_a = 1; assert(func_a() == 1);
+
+    conf_b = 0; assert(func_b() == 0);
+    conf_b = 1; assert(func_b() == 1);
+
+    conf_a = 0; conf_b = 0; assert(func_c() == 0);
+    conf_a = 1; conf_b = 0; assert(func_c() == 1);
+    conf_a = 0; conf_b = 1; assert(func_c() == 1);
+    conf_a = 1; conf_b = 1; assert(func_c() == 1);
 
     return 0;
 }
