f346af27fc1ef4314650040f08df60bb33132462
Valentin Rothberg <valentinrothberg@gmail.com>
2017-02-14 13:16:20 +0100

    tests: no-reference-in-mv-func.c: correct description

--- tests/no-reference-in-mv-func.c
+++ tests/no-reference-in-mv-func.c
@@ -1,21 +1,20 @@
 /*
- * If a multiverse variable is the lvalue of an assignment, it must be removed
- * from the variable setting of the function.
+ * The compiler throws a warning in case a multiverse function does not
+ * reference any multiverse variable.
  */
 
-
-#include <assert.h>
 #include <stdio.h>
-
 #include "multiverse.h"
+#include "testsuite.h"
 
 typedef enum { false, true } bool;
+
 
 void __attribute__((multiverse)) foo()
 {
     for (int i=0; i < 100; i++) {
         // Do nothing
-        i++;
+        printf("i=%d\n", i);
     }
 }
 
@@ -23,8 +22,6 @@ void __attribute__((multiverse)) foo()
 int main(int argc, char **argv)
 {
     multiverse_init();
-
-    multiverse_dump_info(stderr);
 
     foo();
 
