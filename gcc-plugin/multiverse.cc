luhsra/multiverse

commit c3c6d9de105ff4119744319212f96442b42a26b6

multiverse.cc: merge_if_possible: don't fail with disjunct variable vectors
Referencing tracked variables can lead to disjunct variable vectors
while merging.  Just skip them instead of aborting.

gcc-plugin/multiverse.cc
@@ -646,8 +646,8 @@ merge_if_possible(std::vector<var_assign_t> &a,
     bool compatible = false;
     unsigned lower, upper;
     for (unsigned i = 0; i < a.size() ; i++) {
-        assert(a[i].variable->var_decl
-               == b[i].variable->var_decl);
+        if (a[i].variable->var_decl == b[i].variable->var_decl)
+            continue;
         if (a[i].lower_limit == b[i].lower_limit
             && a[i].upper_limit == b[i].upper_limit)
             continue;

commit f10089f0abea7428a955e36a581328b406987499

gcc-plugin: fix regression with descriptor merging
c3c6d9de introduced a regression that disabled all descriptor merging
operations. Therefore, the mvfn-reduction failed. This change fixes
the regression by disallowing merge operations on incompatible
assignment vectors.

gcc-plugin/multiverse.cc
@@ -648,13 +648,16 @@ merge_if_possible(std::vector<var_assign_t> &a,
                   std::vector<var_assign_t> &b)
 {
     if (a.size() != b.size()) return false;
+    // FIXME: Some sort of variable sorting here to normalize the assignment maps
 
     unsigned differences = 0, idx = 0;
     bool compatible = false;
     unsigned lower, upper;
     for (unsigned i = 0; i < a.size() ; i++) {
-        if (a[i].variable->var_decl == b[i].variable->var_decl)
-            continue;
+        // If two assignments have not exactly the same variable
+        // order, we cannot compare them.
+        if (a[i].variable->var_decl != b[i].variable->var_decl)
+            return false;
         if (a[i].lower_limit == b[i].lower_limit
             && a[i].upper_limit == b[i].upper_limit)
             continue;
@@ -667,6 +670,8 @@ merge_if_possible(std::vector<var_assign_t> &a,
             compatible = true;
         }
     }
+    // We are only allowed to touch the a assignment, iff we are sure
+    // merging worked.
     if (differences == 1 && compatible) {
         a[idx].lower_limit = lower;
         a[idx].upper_limit = upper;
@@ -688,8 +693,9 @@ static
 int merge_mvfn_selectors(func_t &fn_info,
                          equivalence_class &ec)
 {
-    debug_printf("\nmerge mvfn descriptors for: %s\n",
-                 IDENTIFIER_POINTER(DECL_NAME(fn_info.fn_decl)));
+    debug_printf("\nmerge mvfn descriptors for: %s starting with %d\n",
+                 IDENTIFIER_POINTER(DECL_NAME(fn_info.fn_decl)),
+                 ec.size());
 
     bool changed = true;
     while (changed) {