324a7c3d11f541e1c71c8ee5b3efebbc62337e0d
Florian Rommel <flo@usrdev.de>
2017-06-04 01:56:35 +0200

    Update kernel module test

--- kmod/multiverse_test.c
+++ kmod/multiverse_test.c
@@ -1,15 +1,33 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/multiverse.h>
 
 MODULE_LICENSE("GPL");
 
-void multiverse_fn(void);
+static __attribute__((multiverse)) bool config_A = false;
+static __attribute__((multiverse)) bool config_B = false;
+
+void __attribute__((multiverse)) foo(void) {
+    label:
+    printk(KERN_INFO "foo at: %p\n", &&label);
+    if (config_A)
+        printk(KERN_INFO ">>> config_A\n");
+    if (config_B)
+        printk(KERN_INFO ">>> config_B\n");
+}
 
 static int multiverse_test_init_module(void) {
     printk(KERN_INFO "Hello from multiverse_test.\n");
 
-    multiverse_fn();
+    multiverse_init();
+
+    config_A = true;
+    multiverse_commit_refs(&config_A);
+    foo();
+    config_B = true;
+    multiverse_commit_refs(&config_B);
+    foo();
 
     return 0;
 }
