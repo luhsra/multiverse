fe25778251986ce24c2f3fe506c8672a5caa9bbe
Florian Rommel <flo@usrdev.de>
2017-07-23 16:59:10 +0200

    libmultiverse: fix userspace cflags

--- libmultiverse/user.mk
+++ libmultiverse/user.mk
@@ -1,6 +1,6 @@
 MY_CC ?= gcc
 CC = $(MY_CC)
-CFLAGS = -gdwarf-2 -Wall -Wextra -O2 -std=c99 -fno-common -mcmodel=kernel
+CFLAGS = -gdwarf-2 -Wall -Wextra -O2 -std=c99 -fno-common
 ALLPRODUCTS+=libmultiverse.a
 
 all: libmultiverse.a
