70033ac326dfd3969bc86c80018e46a70a2edc5e
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2017-09-13 21:25:08 +0200

    octopus: move location of multiverse in source tree

--- octopos-multiverse.mk
+++ octopos-multiverse.mk
@@ -1,15 +1,19 @@
 $(shell echo Multiverse support was loaded >&2)
 
-MULT_DIR=${SRC_DIR}/multiverse-gcc-plugin
-MULT_PLUGIN=${MULT_DIR}/multiverse.so
+MULT_DIR=${SRC_DIR}/multiverse
+MULT_PLUGIN_DIR=${MULT_DIR}/gcc-plugin
+MULT_PLUGIN=${MULT_PLUGIN_DIR}/multiverse.so
 MULT_PLUGIN_SRC=$(shell find ${MULT_PLUGIN_DIR} -name "*.cc"  2> /dev/null)
+
+MULT_LIB_DIR=${MULT_DIR}/libmultiverse
+
 
 MODULES_ALL += multiverse
 MODULES_CLEAN += multiverse
 
-KERNEL_FLAGS += -I$(SRC_DIR)/libmultiverse -fplugin=${MULT_PLUGIN}
+KERNEL_FLAGS += -I${MULT_LIB_DIR} -fplugin=${MULT_PLUGIN}
 
-MULT_C_SRC   = $(shell find $(SRC_DIR)/libmultiverse -name "*.c"  2> /dev/null)
+MULT_C_SRC   = $(shell find $(MULT_LIB_DIR) -name "*.c"  2> /dev/null)
 MULT_C_OBJ := $(MULT_C_SRC:%=%.o)
 MULT_PREF_OBJ := $(addprefix $(OBJDIR)/,$(MULT_C_OBJ))
 # Include libmultiverse into the kernel
@@ -22,11 +26,11 @@ DEP_FILES += ${MULT_DEP}
 ${OBJDIR}/puma.config: ${MULT_PLUGIN}
 ${MULT_PLUGIN}: ${MULT_PLUGIN_SRC}
 	@echo "Build Multiverse GCC Plugin: ${MULT_PLUGIN}"
-	@${MAKE} -C ${MULT_DIR}
+	@${MAKE} -C ${MULT_PLUGIN_DIR}
 
 clean_multiverse:
 	$(RM) ${MULT_PREF_OBJ}
 
 
 # This module is not part of the OS kernel
-$(eval $(call EXTERNAL_MODULE,libmultiverse))
+$(eval $(call EXTERNAL_MODULE,multiverse/libmultiverse))
