fc84684e6bab3a977ed404bf77e3add59299af6f
Studentenaccount <dom.toellner@lab.sra.uni-hannover.de>
2019-04-29 12:11:09 +0200

    TASK: Extended var/fn search mechanism.

--- libmultiverse/mv_info.c
+++ libmultiverse/mv_info.c
@@ -25,7 +25,9 @@ struct mv_info_callsite *__start___multiverse_callsite_ptr = &__start___multiver
 struct mv_info_callsite *__stop___multiverse_callsite_ptr = &__stop___multiverse_callsite_;
 
 // Global multiverse lists
+extern struct mv_info_var_xt *mv_info_var_xt_list;
 extern struct mv_info_fn_xt *mv_info_fn_xt_list;
+extern struct mv_info_callsite_xt *mv_info_callsite_xt_list;
 
 
 struct mv_info_var *
@@ -37,11 +39,39 @@ multiverse_info_var(void  *variable_location) {
     return NULL;
 }
 
+/**
+ * Searches the variable which lies at variable_location in the modules var section.
+ * It is however possible that the module used an external defined variable
+ * which results in the variable_location being placed not inside this modules var section,
+ * but rather in the var section of the object file which defines the extern variable.
+ * 
+ * Therefor we first search inside the modules own var section and after that 
+ * we iterate over every other variable definition we already registed in our global list.
+ * 
+ * CAUTION:
+ * It may still happen that the object which defines the extern variable is not already loaded
+ * and therefor not registered in our global list. Then this method will fail...
+ */
 struct mv_info_var *multiverse_info_var_module(struct mv_info_var *start, struct mv_info_var *stop, void *variable_location){
     struct mv_info_var *var;
+    struct mv_info_var_xt *global_var_list_iter;
+
+    /* Search in own var section. */
     for (var = start; var < stop; var++) {
         if (var->variable_location == variable_location) return var;
     }
+
+    /* Loop through all variable extensions. */
+    for(global_var_list_iter = mv_info_var_xt_list; global_var_list_iter != NULL; global_var_list_iter = global_var_list_iter->next) {
+        unsigned int i;
+        /* Loop through all variables in the current extension. */
+        for(i = 0; i < global_var_list_iter->mv_info_var_len; i++){
+            struct mv_info_var *current_mv_info_var = global_var_list_iter->mv_info_var[i];
+
+            if(current_mv_info_var->variable_location == variable_location) return current_mv_info_var;
+        }
+    }
+
     return NULL;
 }
 
@@ -54,12 +84,40 @@ multiverse_info_fn(void  *function_body) {
     return NULL;
 }
 
+/**
+ * Searches the fn using the function_body address in the modules fn section.
+ * It is however possible that the module used an external defined function
+ * which results in function_body being placed not inside this modules fn section,
+ * but rather in the fn section of the object file which defines the extern function.
+ * 
+ * Therefor we first search inside the modules own fn section and after that 
+ * we iterate over every other fn definition we already registed in our global list.
+ * 
+ * CAUTION:
+ * It may still happen that the object which defines the extern function is not already loaded
+ * and therefor not registered in our global list. Then this method will fail...
+ */
 struct mv_info_fn *
 multiverse_info_fn_module(struct mv_info_fn *start, struct mv_info_fn *stop, void  *function_body) {
     struct mv_info_fn *fn;
+    struct mv_info_fn_xt *global_fn_list_iter;
+
+    /* Search in own fn section. */
     for (fn = start; fn < stop; fn++) {
         if (fn->function_body == function_body) return fn;
     }
+
+    /* Loop through all fn extensions. */
+    for(global_fn_list_iter = mv_info_fn_xt_list; global_fn_list_iter != NULL; global_fn_list_iter = global_fn_list_iter->next) {
+        unsigned int i;
+        /* Loop through all fn in the current extension. */
+        for(i = 0; i < global_fn_list_iter->mv_info_fn_len; i++){
+            struct mv_info_fn *current_mv_info_fn = global_fn_list_iter->mv_info_fn[i];
+
+            if(current_mv_info_fn->function_body == function_body) return current_mv_info_fn;
+        }
+    }
+
     return NULL;
 }
 
@@ -185,6 +243,14 @@ int multiverse_init_module(
     struct mv_info_fn *fn;
     struct mv_info_callsite *callsite;
 
+    /*multiverse_os_print("Mod_var_start: 0x%llx\n", (unsigned long long)mod_var_start);
+    multiverse_os_print("Mod_var_stop: 0x%llx\n", (unsigned long long)mod_var_stop);
+    multiverse_os_print("Mod_fn_start: 0x%llx\n", (unsigned long long)mod_fn_start);
+    multiverse_os_print("Mod_fn_stop: 0x%llx\n", (unsigned long long)mod_fn_stop);
+    multiverse_os_print("Mod_callsite_start: 0x%llx\n", (unsigned long long)mod_callsite_start);
+    multiverse_os_print("Mod_callsite_stop: 0x%llx\n", (unsigned long long)mod_callsite_stop);*/
+
+
     // Step 2: Connect all the moving parts from all compilation units
     //         and fill the runtime data.
     for (fn = mod_fn_start; fn < mod_fn_stop; fn++) {
@@ -217,6 +283,11 @@ int multiverse_init_module(
                 struct mv_info_assignment *assign = &mvfn->assignments[x];
                 struct mv_info_var* fvar = multiverse_info_var_module(mod_var_start, mod_var_stop, assign->variable.location);
 
+                if(fvar == NULL){
+                    multiverse_os_print("Fvar is null...\n");
+                    multiverse_os_print("Assign address: 0x%llx - variable location: 0x%llx\n", (unsigned long long)assign, (unsigned long long)assign->variable.location);
+                }
+
                 MV_ASSERT(fvar != NULL);
                 assign->variable.info = fvar;
 
@@ -239,7 +310,8 @@ int multiverse_init_module(
         }
     }
 
-    
+    return 0;
+
     for (callsite = mod_callsite_start;
          callsite < mod_callsite_stop; callsite++) {
         struct mv_patchpoint pp;
@@ -263,6 +335,17 @@ int multiverse_init_module(
     return 0;
 }
 
+void multiverse_cleanup_module(
+    struct mv_info_var_xt *mod_var_xt,
+    struct mv_info_fn_xt *mod_fn_xt,
+    struct mv_info_callsite_xt *mod_callsite_xt
+) {
+    // Revoke patchpoint-to-fn-append (mv_info_fn_patchpoint_append).
+
+    // Revoke fn-to-var-append (mv_info_var_fn_append).
+
+}
+
 void multiverse_dump_info(void) {
     /* struct mv_info_var *var; */
     struct mv_info_fn *fn;
@@ -279,17 +362,17 @@ void multiverse_dump_info(void) {
         for (mv_info_fn_xt_index = 0; mv_info_fn_xt_index < fn_xt_iter->mv_info_fn_len; mv_info_fn_xt_index++) {
             int k;
             fn = fn_xt_iter->mv_info_fn[mv_info_fn_xt_index];
-            multiverse_os_print("  fn: %s %p, %d variants\n", /*", %d patchpoints(s)\n",*/
+            multiverse_os_print("  fn: %s 0x%llx, %d variants\n", /*", %d patchpoints(s)\n",*/
                                 fn->name,
-                                fn->function_body,
+                                (unsigned long long)fn->function_body,
                                 fn->n_mv_functions
                                 /*fn->extra->n_patchpoints*/);
             for (k = 0; k < fn->n_mv_functions; k++) {
                 unsigned x;
                 struct mv_info_mvfn * mvfn = &fn->mv_functions[k];
                 // Execute function mv_func();
-                multiverse_os_print("    mvfn: %p (vars %d)",
-                                    mvfn->function_body, mvfn->n_assignments);
+                multiverse_os_print("    mvfn: 0x%llx (vars %d)",
+                                    (unsigned long long)mvfn->function_body, mvfn->n_assignments);
                 if (fn->active_mvfn == mvfn) {
                     multiverse_os_print("<-- active");
                 }
